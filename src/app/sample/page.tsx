adee864ae3d27f3d1a9271588079022c4c7b3c3a
Adriano de Azevedo <contato@adriano.dev>
2025-08-19 21:41:51 -0300

    chore: setups tailwind typography

---
+++ src/app/sample/page.tsx
@@ -0,0 +1,9 @@
+import Sample from './sample.mdx';
+
+export default function SamplePage() {
+  return (
+    <article className="mx-auto prose prose-xl px-4 py-12 prose-invert">
+      <Sample />
+    </article>
+  );
+}
