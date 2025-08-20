f64886648ea55571a28fb8efc61dba91d7aa1253
Adriano de Azevedo <contato@adriano.dev>
2025-08-19 21:20:22 -0300

    feat: adds mdx support

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
+import { NextConfig } from 'next';
+import remarkGfm from 'remark-gfm';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+export default withMDX(nextConfig);
