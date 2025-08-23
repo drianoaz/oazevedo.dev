396a93c7a8f3149eb729200351d79d9eec509ac4
Adriano de Azevedo <contato@adriano.dev>
2025-08-23 17:59:44 -0300

    feat: adds post header

--- src/app/blog/[slug]/blog-content.tsx
+++ src/app/blog/[slug]/blog-content.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { CalendarIcon, ClockIcon } from 'lucide-react';
 import { MDXClient } from 'next-mdx-remote-client';
 import type { SerializeResult } from 'next-mdx-remote-client/serialize';
 import { components } from '@/components/mdx';
@@ -17,15 +18,48 @@ export function BlogContent({ mdxSource }: ContentProps) {
     });
   }
 
-  return (
-    <div className="mx-auto md:grid md:grid-cols-[1fr_auto_1fr] md:items-start md:justify-end">
-      <div className="max-md:hidden"></div>
+  const { frontmatter, scope } = mdxSource;
 
-      <article className="mx-auto prose prose-xl p-4 prose-invert">
+  return (
+    <main className="mx-auto grid grid-cols-[1fr_auto_1fr]">
+      <header className="col-start-2 col-end-3 mx-auto prose prose-xl px-4 py-12 prose-invert">
+        <h1>{frontmatter.title}</h1>
+        {frontmatter.summary && (
+          <p className="text-left text-lg text-gray-300">
+            {frontmatter.summary}
+          </p>
+        )}
+        <div className="flex flex-wrap gap-4 text-sm text-gray-400">
+          {frontmatter.publishedAt && (
+            <time
+              dateTime={frontmatter.publishedAt}
+              className="flex items-center gap-2"
+            >
+              <CalendarIcon className="size-4" />
+              <span>
+                {frontmatter.publishedAt.split('-').reverse().join('/')}
+              </span>
+            </time>
+          )}
+
+          {scope.readingTime && (
+            <span className="flex items-center gap-2">
+              <ClockIcon className="size-4" />
+              <span>
+                {Math.round(scope.readingTime.minutes)} min. de leitura
+              </span>
+            </span>
+          )}
+        </div>
+      </header>
+
+      <article className="col-start-2 row-start-2 mx-auto prose prose-xl px-4 pb-4 prose-invert">
         <MDXClient {...mdxSource} components={components} />
       </article>
 
-      <TableOfContents toc={mdxSource.scope.toc ?? []} />
-    </div>
+      <aside className="col-start-3 row-start-2">
+        <TableOfContents toc={mdxSource.scope.toc ?? []} />
+      </aside>
+    </main>
   );
 }
