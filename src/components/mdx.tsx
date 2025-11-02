fd86e3d1211b776280e31aea4c47920007ed2ae9
Adriano de Azevedo <contato@adriano.dev>
2025-11-01 21:20:38 -0300

    fix: blog layout

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -3,4 +3,9 @@ import CodeBlock from './code-block';
 
 export const components: MDXComponents = {
   pre: CodeBlock,
+  table: (props: React.HTMLAttributes<HTMLTableElement>) => (
+    <div className="my-9 w-full overflow-x-auto">
+      <table {...props} />
+    </div>
+  ),
 };
